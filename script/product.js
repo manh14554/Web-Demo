89bb6a2d470eaf61919f53e64f76a93ce49131be
manh14554 <manhwqea@gmail.com>
2026-04-21 11:29:53 +0700

    Cập nhật các liên kết mạng xã hội và cải thiện mô tả sản phẩm

--- script/product.js
+++ script/product.js
@@ -1,37 +1,37 @@
 const productsData = {
     "balo": {
         name: "Swag Labs Backpack",
-        desc: "Carry all the things with the sleek, streamlines Sly Pack. It features a roomy main compartment, a front zip pocket for your smaller items, and a mesh water bottle holder on the side.",
+        desc: "Carry all the things with the sleek, streamlined Sly Pack that melds uncompromising style with unequaled laptop and tablet protection.",
         price: "$29.99",
         image: "./images/balo.png"
     },
     "ao-len": {
         name: "Red Long Sleeve T-Shirt",
-        desc: "This classic Swag Labs T-Shirt is perfect to wear when cozying up to your keyboard. Super-soft and comfy ringspun combed cotton.",
+        desc: "This classic Swag Labs t-shirt is perfect to wear when cozying up to your keyboard to automate a few tests. Super-soft and comfy ringspun combed cotton.",
         price: "$29.99",
         image: "./images/ao-len.png"
     },
     "den": {
         name: "Bike Lights",
-        desc: "A red light isn't the desired state testing but it sure helps when riding your bike at night.",
+        desc: "A red light isn't the desired state in testing but it sure helps when riding your bike at night. Water-resistant with 3 lighting modes, 1 AAA battery included.",
         price: "$9.99",
         image: "./images/den.png"
     },
     "ao-thun": {
         name: "Bolt T-Shirt",
-        desc: "Get your testing superhero on with the Swag Labs bolt T-Shirt. From our best-selling Bolt T-Shirt to the classic Short Sleeve Tee, you'll be sure to get a jolt out of your day wearing this soft cotton tee.",
+        desc: "Get your testing superhero on with the Swag Labs bolt T-shirt. From American Apparel, 100% ringspun combed cotton, heather gray with white print.",
         price: "$15.99",
         image: "./images/ao-thun.png"
     },
     "ao-khoac": {
         name: "Swag Labs Fleece Jacket",
-        desc: "It's not everyday that you come across midweight quarter-zip fleece jacket capable of handling everything from a relaxing cruise around the park to a busy day at the office.",
+        desc: "It's not every day that you come across a midweight quarter-zip fleece jacket capable of handling everything from a relaxing day outdoors to a busy day at the office.",
         price: "$29.99",
         image: "./images/ao-khoac.png"
     },
     "ao": {
         name: "Swag Labs Onesie",
-        desc: "Rip snap infant onesie for the junior automation engineer in development. Reinforced 3-snap bottom closure, two-needle hemmed sleeved and bottom won't unravel.",
+        desc: "Rib snap infant onesie for the junior automation engineer in development. Reinforced 3-snap bottom closure, two-needle hemmed sleeved and bottom won't unravel.",
         price: "$19.99",
         image: "./images/ao.png"
     }
